bc761aaf399fb016379e512b4524ee935f91f061
Alex Papworth <arpapworth@gmail.com>
2019-07-01 15:18:57 +0100

    Build base framework for API

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Usage
 
-Clone
-cd
+Clone this project.
+
+```ruby
+git clone git@github.com:alexpapworth/fizzbuzz-app.git
+```
+
+Install the dependencies
+
+```ruby
+cd fizzbuzz-api
+npm install fs
+npm install request
 npm install minimist
+```
+
+Run the API.
+
+```ruby
+node api.js show favourites --number 8
+```
--- api.js
+++ api.js
@@ -1,24 +1,74 @@
+const fs = require('fs');
+const request = require('request');
 const argv = require('minimist')(process.argv.slice(2));
-console.log(argv.id);
+// console.log(argv.id);
+
+// fs.writeFile(__dirname+"auth_token", "Hey there!", function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+
+//     console.log("The file was saved!");
+// }); 
+
+var auth_token;
+
+getAuthToken = new Promise(function(resolve, reject) {
+	file = __dirname+"/auth_token";
+	fs.access(file, fs.constants.F_OK, (err) => {
+	  if (err) {
+	  	resolve("");
+	  } else {
+	  	fs.readFile(file, "utf8", (err, data) => {
+		  if (err) throw err;
+		  console.log('got the token');
+		  data = data.replace(/\n$/, '')
+		  resolve(data);
+		});
+	  }
+	});
+});
+
+
+async function sendGetRequest(path) {
+	getAuthToken.then(function(auth_token) {
+		console.log(auth_token);
+		options = {
+			url: 'http://localhost:3000/api/v1/' + path,
+			headers: {
+				Cookie: 'auth_token='+auth_token
+			},
+			json: true
+		};
+		console.log(options);
+		request.get(options, (err, res, body) => {
+			if (err) { return console.log(err); }
+			console.log(body);
+		});
+	});
+}
+
 
 if (argv._[0] == 'show') {
 	switch(argv._[1]) {
 		case "favourites":
 			if (argv.id) {
-
+				sendGetRequest("favourite/show/id"+argv.id);
 			} else if (argv.name) {
-				showFavourites();
+				sendGetRequest("favourite/show/name"+argv.name);
 			} else if (argv.number) {
-				showFavourites();
+				sendGetRequest("favourite/show/number"+argv.number);
 			} else {
-				if (false) {
-					showFavourites();
-				} else {
-					console.log("\nNo arguments given for `show favourite`\n");
-					console.log(" Choose a user using --id or --name");
-					console.log(" Or choose a number using --number");
-					console.log(" Or run `create session` to use `show favourite` without arguments\n");
-				}
+				getAuthToken.then(function(auth_token) {
+					if (auth_token.length > 10) {
+						sendGetRequest("favourite/show");
+					} else {
+						console.log("\nNo arguments given for `show favourite`\n");
+						console.log(" Choose a user using --id or --name");
+						console.log(" Or choose a number using --number");
+						console.log(" Or run `create session` to use `show favourite` without arguments\n");
+					}
+				});
 			}
 		break;
 
