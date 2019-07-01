94a09ae3f12bebbce256fb6060f890a67fdc181f
Alex Papworth <arpapworth@gmail.com>
2019-07-01 16:51:42 +0100

    Add more API commands

--- api.js
+++ api.js
@@ -30,7 +30,43 @@ getAuthToken = new Promise(function(resolve, reject) {
 });
 
 
+async function sendDeleteRequest(path) {
+	getAuthToken.then(function(auth_token) {
+		console.log(auth_token);
+		options = {
+			url: 'http://localhost:3000/api/v1/' + path,
+			headers: {
+				Cookie: 'auth_token='+auth_token
+			},
+			json: true
+		};
+		console.log(options);
+		request.delete(options, (err, res, body) => {
+			if (err) { return console.log(err); }
+			console.log(body);
+		});
+	});
+}
+
 async function sendGetRequest(path) {
+	getAuthToken.then(function(auth_token) {
+		console.log(auth_token);
+		options = {
+			url: 'http://localhost:3000/api/v1/' + path,
+			headers: {
+				Cookie: 'auth_token='+auth_token
+			},
+			json: true
+		};
+		console.log(options);
+		request.post(options, (err, res, body) => {
+			if (err) { return console.log(err); }
+			console.log(body);
+		});
+	});
+}
+
+async function sendPostRequest(path) {
 	getAuthToken.then(function(auth_token) {
 		console.log(auth_token);
 		options = {
@@ -49,15 +85,60 @@ async function sendGetRequest(path) {
 }
 
 
+if (argv._[0] == 'create') {
+	switch(argv._[1]) {
+		case "favourite":
+			
+		break;
+
+		case "session":
+			
+		break;
+
+		case "user":
+			if (argv.name) {
+				sendPostRequest("user/create/name/"+argv.name);
+			} else {
+				sendPostRequest("user/create");
+			}
+		break;
+
+	}
+}
+
+if (argv._[0] == 'destroy') {
+	switch(argv._[1]) {
+		case "favourite":
+			
+		break;
+
+		case "session":
+			
+		break;
+
+		case "user":
+			if (argv.id) {
+				sendDeleteRequest("user/destroy/id/"+argv.id);
+			} else if (argv.name) {
+				sendDeleteRequest("user/destroy/name/"+argv.name);
+			} else {
+				console.log("\nNo arguments given for `destroy user`\n");
+				console.log(" Select a user using --id or --name\n");
+			}
+		break;
+
+	}
+}
+
 if (argv._[0] == 'show') {
 	switch(argv._[1]) {
 		case "favourites":
 			if (argv.id) {
-				sendGetRequest("favourite/show/id"+argv.id);
+				sendGetRequest("favourite/show/id/"+argv.id);
 			} else if (argv.name) {
-				sendGetRequest("favourite/show/name"+argv.name);
+				sendGetRequest("favourite/show/name/"+argv.name);
 			} else if (argv.number) {
-				sendGetRequest("favourite/show/number"+argv.number);
+				sendGetRequest("favourite/show/number/"+argv.number);
 			} else {
 				getAuthToken.then(function(auth_token) {
 					if (auth_token.length > 10) {
@@ -73,23 +154,53 @@ if (argv._[0] == 'show') {
 		break;
 
 		case "number":
-			
+			if (argv.id) {
+				sendGetRequest("number/show/id/"+argv.id);
+			} else {
+				console.log("\nNo arguments given for `show number`\n");
+				console.log(" Choose a number using --id");
+				console.log(" Or show all numbers using `show numbers`");
+			}
 		break;
 
 		case "numbers":
-			
+			if (argv.page && argv.size) {
+				sendGetRequest("number/show/page/"+argv.page+"/size/"+argv.size);
+			} else if (argv.page) {
+				sendGetRequest("number/show/page/"+argv.page);
+			} else if (argv.page) {
+				sendGetRequest("number/show/size/"+argv.size);
+			} else {
+				sendGetRequest("number/show/");
+			}
 		break;
 
 		case "session":
-			
+				getAuthToken.then(function(auth_token) {
+					if (auth_token.length > 10) {
+						sendGetRequest("session/show/");
+					} else {
+						console.log("\nLooks like there aren't any sessions enabled yet.`\n");
+						console.log(" Run `create session --id 1`");
+						console.log(" Or `create session --name HealthyCactusPlant` to create one");
+					}
+				});
 		break;
 
 		case "user":
-			
+			if (argv.id) {
+				sendGetRequest("user/show/id/"+argv.id);
+			} else if (argv.name) {
+				sendGetRequest("user/show/name/"+argv.name);
+			} else {
+				console.log("\nNo arguments given for `show user`\n");
+				console.log(" Choose a user using --id or --name");
+				console.log(" Or show all users using `show users`\n");
+			}
 		break;
 
 		case "users":
-			
+			sendGetRequest("user/show/");
 		break;
 
 	}
