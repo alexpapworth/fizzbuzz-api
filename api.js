b2b76ad50a6b32a2325023c97225fba2fd7df2d8
Alex Papworth <arpapworth@gmail.com>
2019-07-02 00:22:33 +0100

    Complete API commands, add help section

--- api.js
+++ api.js
@@ -32,7 +32,7 @@ getAuthToken = new Promise(function(resolve, reject) {
 
 async function sendDeleteRequest(path) {
 	getAuthToken.then(function(auth_token) {
-		console.log(auth_token);
+		console.log("auth_token:" + auth_token);
 		options = {
 			url: 'http://localhost:3000/api/v1/' + path,
 			headers: {
@@ -41,16 +41,24 @@ async function sendDeleteRequest(path) {
 			json: true
 		};
 		console.log(options);
+		console.log();
 		request.delete(options, (err, res, body) => {
 			if (err) { return console.log(err); }
 			console.log(body);
+			if (eval(body).auth_token != undefined && eval(body).auth_token.length == 0) {
+				file = __dirname+"/auth_token";
+
+				fs.unlink(file, (err) => {
+					if (err) throw err;
+				});
+			}
 		});
 	});
 }
 
 async function sendGetRequest(path) {
 	getAuthToken.then(function(auth_token) {
-		console.log(auth_token);
+		console.log("auth_token:" + auth_token);
 		options = {
 			url: 'http://localhost:3000/api/v1/' + path,
 			headers: {
@@ -59,7 +67,8 @@ async function sendGetRequest(path) {
 			json: true
 		};
 		console.log(options);
-		request.post(options, (err, res, body) => {
+		console.log();
+		request.get(options, (err, res, body) => {
 			if (err) { return console.log(err); }
 			console.log(body);
 		});
@@ -68,7 +77,7 @@ async function sendGetRequest(path) {
 
 async function sendPostRequest(path) {
 	getAuthToken.then(function(auth_token) {
-		console.log(auth_token);
+		console.log("auth_token:" + auth_token);
 		options = {
 			url: 'http://localhost:3000/api/v1/' + path,
 			headers: {
@@ -77,9 +86,19 @@ async function sendPostRequest(path) {
 			json: true
 		};
 		console.log(options);
-		request.get(options, (err, res, body) => {
+		console.log();
+		request.post(options, (err, res, body) => {
 			if (err) { return console.log(err); }
 			console.log(body);
+			if (eval(body).auth_token) {
+				file = __dirname+"/auth_token";
+
+			  	fs.writeFile(file, eval(body).auth_token, 'utf8', (err) => {
+				  if (err) throw err;
+				  console.log('\n  Wrote the auth_token to local system. You many now run `show favourites` and `create/destroy favourite`');
+				});
+
+			}
 		});
 	});
 }
@@ -88,11 +107,24 @@ async function sendPostRequest(path) {
 if (argv._[0] == 'create') {
 	switch(argv._[1]) {
 		case "favourite":
-			
+			if (argv.number) {
+				sendPostRequest("favourite/create/number/"+argv.number);
+			} else {
+				console.log("\nNo arguments given for `create favourite`\n");
+				console.log(" Select a number using --number\n");
+			}
 		break;
 
 		case "session":
-			
+			if (argv.id) {
+				sendPostRequest("session/create/id/"+argv.id);
+			} else if (argv.name) {
+				sendPostRequest("session/create/name/"+argv.name);
+
+			} else {
+				console.log("\nNo arguments given for `create session`\n");
+				console.log(" Select a user using --id or --name\n");
+			}
 		break;
 
 		case "user":
@@ -109,11 +141,24 @@ if (argv._[0] == 'create') {
 if (argv._[0] == 'destroy') {
 	switch(argv._[1]) {
 		case "favourite":
-			
+			if (argv.number) {
+				sendDeleteRequest("favourite/destroy/number/"+argv.number);
+			} else {
+				console.log("\nNo arguments given for `destroy favourite`\n");
+				console.log(" Select a number using --number\n");
+			}
 		break;
 
 		case "session":
-			
+			getAuthToken.then(function(auth_token) {
+				if (auth_token.length > 10) {
+					sendDeleteRequest("session/destroy/");
+				} else {
+					console.log("\nLooks like there aren't any sessions to destroy.`\n");
+					console.log(" Run `create session --id 1`");
+					console.log(" Or `create session --name HealthyCactusPlant` to create one");
+				}
+			});
 		break;
 
 		case "user":
@@ -132,13 +177,17 @@ if (argv._[0] == 'destroy') {
 
 if (argv._[0] == 'show') {
 	switch(argv._[1]) {
+		case "favourite":
+			console.log("\nNo command found for `show favourite`. Did you mean `show favourites`?");
+		break;
+
 		case "favourites":
 			if (argv.id) {
-				sendGetRequest("favourite/show/id/"+argv.id);
+				sendGetRequest("favourites/show/id/"+argv.id);
 			} else if (argv.name) {
-				sendGetRequest("favourite/show/name/"+argv.name);
+				sendGetRequest("favourites/show/name/"+argv.name);
 			} else if (argv.number) {
-				sendGetRequest("favourite/show/number/"+argv.number);
+				sendGetRequest("favourites/show/number/"+argv.number);
 			} else {
 				getAuthToken.then(function(auth_token) {
 					if (auth_token.length > 10) {
@@ -165,13 +214,13 @@ if (argv._[0] == 'show') {
 
 		case "numbers":
 			if (argv.page && argv.size) {
-				sendGetRequest("number/show/page/"+argv.page+"/size/"+argv.size);
+				sendGetRequest("numbers/show/page/"+argv.page+"/size/"+argv.size);
 			} else if (argv.page) {
-				sendGetRequest("number/show/page/"+argv.page);
-			} else if (argv.page) {
-				sendGetRequest("number/show/size/"+argv.size);
+				sendGetRequest("numbers/show/page/"+argv.page);
+			} else if (argv.size) {
+				sendGetRequest("numbers/show/size/"+argv.size);
 			} else {
-				sendGetRequest("number/show/");
+				sendGetRequest("numbers/show/");
 			}
 		break;
 
@@ -200,15 +249,46 @@ if (argv._[0] == 'show') {
 		break;
 
 		case "users":
-			sendGetRequest("user/show/");
+			sendGetRequest("users/show/");
 		break;
 
 	}
 }
-
-if (argv.help == true) {
+if (argv.version == true) {
+	console.log("Fizzbuzz cli api v1.0.0");
+} else if (argv.help == true || argv._.length == 0) {
 	var help = "Usage: api.js <command> [options]\n\n";
-	help += "Commands:\n\tapi.js show user  | Show information about users";
+	help += "Commands:";
+	help += "\n  node api.js show favourites\t\tShow information about favourites";
+	help += "\n  node api.js show numbers\t\tShow all numbers";
+	help += "\n  node api.js show number\t\tShow information about a single number";
+	help += "\n  node api.js show session\t\tShow information about current session";
+	help += "\n  node api.js show users\t\tShow all users ";
+	help += "\n  node api.js show user\t\t\tShow information about a single user";
+	help += "\n\nOptions:";
+	help += "\n  --version\tShow version number";
+	help += "\n  --help\tShow help";
+	help += "\n\nExamples:";
+	help += "\n  node api.js show users\t\t\t\tlist of all users";
+	help += "\n  node api.js show user --id 1\t\t\t\tshow user with id 1";
+	help += "\n  node api.js show user --name HealthyCactusPlant\tshow user with name of HealthyCactusPlant";
+	help += "\n  node api.js create user\t\t\t\tcreate user with random name";
+	help += "\n  node api.js create user --name HealthyCactusPlant\tcreate user with name of HealthyCactusPlant";
+	help += "\n  node api.js destroy user --id 1\t\t\tdestroy user with id 1";
+	help += "\n  node api.js destroy user --name HealthyCactusPlant\tdestroy user with name of HealthyCactusPlant";
+	help += "\n\n  node api.js show session\t\t\t\tshow session for this cli";
+	help += "\n  node api.js create session --id 1\t\t\tcreate session for user with id 1";
+	help += "\n  node api.js create session --name HealthyCactusPlant\tcreate session for user with name HealthyCactusPlant";
+	help += "\n  node api.js destroy session --id 1\t\t\tshow session for this cli";
+	help += "\n\n  node api.js show number --id 1\t\t\tshow number with id 1";
+	help += "\n  node api.js show numbers\t\t\t\tshow all numbers";
+	help += "\n  node api.js show numbers --page 1 --size 100\t\tshow 100 numbers starting at page 1";
+	help += "\n\n  node api.js show favourites\t\t\t\tshow favourites for current session user";
+	help += "\n  node api.js show favourites --id 1\t\t\tshow favourites for user with id 1";
+	help += "\n  node api.js show favourites --name HealthyCactusPlant\tshow favourites for user with name HealthyCactusPlant";
+	help += "\n  node api.js create favourite --number 1\t\tcreate favourite for current session user for number 1";
+	help += "\n  node api.js destroy favourite --number 1\t\tdestroy favourite for current session user for number 1";
+	help += "\n\n";
 	console.log(help);
 }
 
--- routes.md
+++ routes.md
@@ -15,8 +15,8 @@ destroy user (id / name)
 	node api.js show user --name HealthyCactusPlant = show user with name of HealthyCactusPlant
 	node api.js create user = create user with random name
 	node api.js create user --name HealthyCactusPlant = create user with name of HealthyCactusPlant
-node api.js destroy user --id 1 = destroy user with id 1
-node api.js destroy user --name HealthyCactusPlant = destroy user with name of HealthyCactusPlant
+	node api.js destroy user --id 1 = destroy user with id 1
+	node api.js destroy user --name HealthyCactusPlant = destroy user with name of HealthyCactusPlant
 
 Sessions
 ========
@@ -25,10 +25,9 @@ create session (id / name) (stored in local variable?)
 destroy session (id / name)
 
 	node api.js show session = session for this cli
-node api.js create session --id 1 = create session for user with id 1
-node api.js create session --name HealthyCactusPlant = create session for user with name HealthyCactusPlant
-node api.js destroy session --id 1 = destroy session for user with id 1
-node api.js destroy session --name HealthyCactusPlant = destroy session for user with name HealthyCactusPlant
+	node api.js create session --id 1 = create session for user with id 1
+	node api.js create session --name HealthyCactusPlant = create session for user with name HealthyCactusPlant
+	node api.js destroy session = destroy current session
 
 Numbers
 =======
@@ -45,8 +44,8 @@ show favourites (id / name)
 	node api.js show favourites = show favourites for current session user
 	node api.js show favourites --id 1 = show favourites for user with id 1
 	node api.js show favourites --name HealthyCactusPlant = show favourites for user with name HealthyCactusPlant
-node api.js create favourite --number 1 = create favourite for current session user for number 1
-node api.js destroy favourite --number 1 = destroy favourite for current session user for number 1
+	node api.js create favourite --number 1 = create favourite for current session user for number 1
+	node api.js destroy favourite --number 1 = destroy favourite for current session user for number 1
 
 @turn into executable
 https://scotch.io/@Youngestdev/how-to-build-a-nodejs-commandline-apps-with-yargs
